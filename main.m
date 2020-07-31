6971a08519803be670eb571dcce67d0d192770b8
Arshia <arshia.rezaee@yahoo.com>
2020-07-31 18:13:43 +0430

    cfghjkm.m removed

--- main.m
+++ main.m
@@ -479,136 +479,14 @@ disp(Counter);
 
 %plot(queue1InterArrival,unique(queue1InterArrival));
 
-function [] = plot_7_1(q1arrive,receptionArrive,coronaTest)
-%PLOT_7_1 Summary of this function goes here
-%   Detailed explanation goes here
-response_time = receptionArrive - q1arrive;
-positive_rosponse_time  = [];
-p_counter=1;
-negative_rosponse_time  = [];
-n_counter=1;
-number_of_patients = length(coronaTest);
-for i = 1:number_of_patients
-    if coronaTest(i)==1
-        positive_rosponse_time(p_counter) = -q1arrive(p_counter)+receptionArrive(p_counter);
-        p_counter = p_counter + 1;
-    elseif coronaTest(i)==0
-        negative_rosponse_time(n_counter) = -q1arrive(n_counter)+receptionArrive(n_counter);
-        n_counter = n_counter + 1;
-    end
-end
-
-figure
-hist(response_time);
- 
-legend('total response time')
- 
-figure
-hist(positive_rosponse_time);
- 
-legend('positive response time')
-figure
-hist(negative_rosponse_time);
- 
-legend('negative response time')
- 
-% plot(negative_rosponse_time,unique(negativee_rosponse_time));
-% hold;
-% 
-% plot(response_time,unique(response_time));
-% hold;
- 
- 
-end
 
 
 
-function [] = plot_7_2(wait1,wait2,coronaTest)
-%PLOT_7_2 Summary of this function goes here
-%   Detailed explanation goes here
-wait = wait1 + wait2;
-positive_wait = [];
-p_counter = 1;
-negative_wait = [];
-n_counter = 1;
-number_of_patients = length(coronaTest);
-for i = 1:number_of_patients
-    if coronaTest(i)==1
-        positive_wait(p_counter) = wait1(p_counter) + wait2(p_counter);
-        p_counter = p_counter + 1;
-    elseif coronaTest(i)==0
-        negative_wait(n_counter) = wait1(n_counter) + wait2(n_counter);
-        n_counter = n_counter + 1;
-    end
-end
 
-figure
-hist(wait);
- 
-legend('total waited time')
- 
-figure
-hist(positive_wait);
- 
-legend('+ corona waited time')
-figure
-hist(negative_wait);
- 
-legend('- corona waited time')
- 
-% plot(negative_rosponse_time,unique(negativee_rosponse_time));
-% hold;
-% 
-% plot(response_time,unique(response_time));
-% hold;
- 
- 
-end
 
 
 
 %numberOfPatientsinSystem(t) = positive_numberOfPatientsinSystem(t) + negative_numberOfPatientsinSystem(t)
-function [] = plot_7_3(numberOfPatientsinSystem,positive_numberOfPatientsinSystem,negative_numberOfPatientsinSystem,coronaTest)
-%PLOT_7_3 Summary of this function goes here
-%   Detailed explanation goes here
-wait = wait1 + wait2;
-positive_wait = [];
-p_counter = 1;
-negative_wait = [];
-n_counter = 1;
-number_of_patients = length(coronaTest);
-for i = 1:number_of_patients
-    if coronaTest(i)==1
-        positive_wait(p_counter) = wait1(p_counter) + wait2(p_counter);
-        p_counter = p_counter + 1;
-    elseif coronaTest(i)==0
-        negative_wait(n_counter) = wait1(n_counter) + wait2(n_counter);
-        n_counter = n_counter + 1;
-    end
-end
-
-figure
-hist(wait);
- 
-legend('total waited time')
- 
-figure
-hist(positive_wait);
- 
-legend('+ corona waited time')
-figure
-hist(negative_wait);
- 
-legend('- corona waited time')
- 
-% plot(negative_rosponse_time,unique(negativee_rosponse_time));
-% hold;
-% 
-% plot(response_time,unique(response_time));
-% hold;
- 
- 
-end
 
 
 
